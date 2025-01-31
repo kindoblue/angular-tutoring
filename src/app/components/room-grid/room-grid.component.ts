a957b2533740c0a876952684731bec562eb9a56f
Stefano <kindoblue@gmail.com>
2025-01-31 14:53:43 +0100

    some style for the room

--- src/app/components/room-grid/room-grid.component.ts
+++ src/app/components/room-grid/room-grid.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FloorService } from '../../services/floor.service';
 import { Floor } from '../../interfaces/floor.interface';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-room-grid',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule
+  ],
   templateUrl: './room-grid.component.html',
   styleUrls: ['./room-grid.component.scss']
 })
